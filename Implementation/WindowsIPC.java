e5128e95dce94b4b6999bd8789cd72925eaf4972
dylsmith8 <dylan.smith888@gmail.com>
2016-05-05 15:36:46 +0200

    InitialClassCreation

---
+++ Implementation/WindowsIPC.java
@@ -0,0 +1,8 @@
+/**   This class creates the Windows IPC functions using a JNI shared library
+  *   @author Dylan Smith
+  *   Start Date: 5 May 2016
+  */
+
+public class WindowsIPC {
+
+} // class
