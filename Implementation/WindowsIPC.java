8cec6eeeb8b068010e1d0c5fa99ec0ffe76921de
dylsmith8 <dylan.smith888@gmail.com>
2016-05-05 16:23:22 +0200

    Created some native functions. Created a list of required methods

--- Implementation/WindowsIPC.java
+++ Implementation/WindowsIPC.java
@@ -2,7 +2,37 @@
   *   @author Dylan Smith
   *   Start Date: 5 May 2016
   */
-
 public class WindowsIPC {
+  /*
+  Calls CreateMailslot()
+  Create a Windows Mailslot. Actual C implementation returns a handle.
+  This function will return -1 if an error occured or 1 if the slot
+  was created successfully.
+  https://msdn.microsoft.com/en-us/library/windows/desktop/aa365147(v=vs.85).aspx
+  http://stackoverflow.com/questions/13060626/mailslot-write-sending-same-thing-three-times-c-c
+  */
+  public native int createMailslot(String name);
+  /*
+  Calls CreatePipe()
+  https://msdn.microsoft.com/en-us/library/bb546102(v=vs.110).aspx
+  Creates an anonymous pipe between processes.
+  Return -1 if failed, 1 if sucessful
+  name is the mailslots name crated by the server
+  */
+  public native int createPipe(String name);
+  /*
+  Calls CreateNamedPipe()
+  https://msdn.microsoft.com/en-us/library/windows/desktop/aa365150(v=vs.85).aspx
+  Return -1 if failed, 1 if sucessful
+  name is the unique name of the named pipe
+  redirects the stdout of the process concerned
+  */
+  public native int createNamedPipe(String name);
 
+  /*
+  Main method. Conduct unit tests here (junit)
+  */
+  public static void Main(String[] args) {
+      WindowsIPC winIPC = new WindowsIPC();
+  }
 } // class
