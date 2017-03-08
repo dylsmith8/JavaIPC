fc0a249f72c75cecd4e5aca6ad7b3300070ac0e3
Dylan Smith <dylan.smith888@gmail.com>
2017-03-08 18:58:03 +0200

    Further cleanups

---
+++ Windows Implementation/clean.bat
@@ -0,0 +1,14 @@
+rem Author: Dylan Smith 
+rem Date: 8 March 2017 
+
+REM move to current workspace
+cd "C:\Users\Dylan\Documents\GitHub\JavaIPC\Windows Implementation"
+
+del *.class
+del WindowsIPC.dll
+del WindowsIPC.lib
+del WindowsIPC.obj
+del WindowsIPC.exp
+del WindowsIPC.h
+del *.log
+pause
